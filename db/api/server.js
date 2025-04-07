e53730d15e0c120c02ecea123327f9c25118b231
Sifa Gakeni <gakenisifa@gmail.com>
2025-04-07 09:10:18 +0300

    updated server file

--- db/api/server.js
+++ db/api/server.js
@@ -6,10 +6,17 @@ const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
-server.use(router);
+server.use(cors());
+server.use(middlewares());
 
+// Explicit route for /patients
+server.get("/patients", (req, res) => {
+  const db = router.db; // this is where your db.json is stored
+  res.jsonp(db.get("patients").value());
+});
+
+// Make sure all other routes are handled by json-server
+server.use(router);
 // const PORT = process.env.PORT || 3000;
 
 // server.listen(PORT, () => {
@@ -26,4 +33,5 @@ server.use(router);
 // server.use(middlewares)
 // server.use(router)
 
-module.exports = server
+module.exports = server;
+
